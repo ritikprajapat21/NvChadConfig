66eae09ce1922629beb5dfe013a36097a3feaa04
ritiprajapat21 <ritipfb21@svvv.edu.in>
2024-03-18 23:01:05 +0530

    Initial commit

---
+++ lua/chadrc.lua
@@ -0,0 +1,15 @@
+---@type ChadrcConfig
+local M = {}
+
+M.ui = {
+  theme = "tokyonight",
+
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
+
+  transparency = true,
+}
+
+return M
---
+++ lua/plugins/init.lua
@@ -0,0 +1,78 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre",
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+        "eslint-lsp",
+        "tailwindcss-language-server",
+        "typescript-language-server",
+        "pyright",
+      },
+    },
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "html",
+        "css",
+        "tsx",
+        "typescript",
+        "javascript",
+      },
+    },
+  },
+
+  {
+    "nvimtools/none-ls.nvim",
+    event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+    config = function()
+      require "configs.none"
+    end,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      highlight = { enable = true },
+      ensure_installed = {
+        "bash",
+        "html",
+        "css",
+        "javascript",
+        "lua",
+        "markdown",
+        "python",
+        "query",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+}
